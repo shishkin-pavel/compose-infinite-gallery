ee6cd3b8972601babfa59d318b2144561d1a3b8f
Shishkin Pavel <shishkin.pasha@gmail.com>
2023-10-04 16:06:30 +0700

    compose gradle config fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ kotlin {
     sourceSets {
         val jvmMain by getting {
             dependencies {
-                implementation(compose.desktop.windows_x64)
+                implementation(compose.desktop.currentOs)
                 implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
 //                implementation("io.ktor:ktor-client-cio:$ktor_version")
